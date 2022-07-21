a98599af6a86fe5079c1d2be572a52cb7c244cc8
refaldy04 <refaldybanggana@gmail.com>
2022-07-22 05:57:20 +0700

    slicing with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Example from './pages/Example';
+// import Example from './pages/Example';
 import Count from './pages/Counter';
+import Home from './pages/Home';
 
 export class App extends Component {
   render() {
     return (
       <Routes>
-        <Route path="/" element={<Example />} />
+        {/* <Route path="/" element={<Example />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="counter" element={<Count />} />
       </Routes>
     );
---
+++ src/pages/Home.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react';
+import '../asset/css/landing-page-2.css';
+import { Link } from 'react-router-dom';
+
+import phone2 from '../asset/img/png-phone-2.png';
+import google from '../asset/img/Group.png';
+import apple from '../asset/img/icon.png';
+
+export class Home extends Component {
+  render() {
+    return (
+      <>
+        <nav>
+          <h1>Zwallet</h1>
+          <div>
+            <Link to="/" className="login">
+              Login
+            </Link>
+            <Link to="/" className="signup">
+              Sign Up
+            </Link>
+          </div>
+        </nav>
+
+        <header>
+          <img src={phone2} class="mobile-display" alt="mobile-display" />
+          <div>
+            <h1>
+              Awesome App For Saving <span class="tagline">Time.</span>
+            </h1>
+            <p>We bring you a mobile app for banking problems that oftenly wasting much of your times.</p>
+            <Link to="/" className="try-it">
+              Try It Free
+            </Link>
+            <span class="text">Available on</span>
+            <div>
+              <img src={google} alt="google-store" class="google" />
+              <img src={apple} alt="apple-store" class="apple" />
+            </div>
+          </div>
+        </header>
+
+        <section class="clients">
+          <img src="asset/img/Group 20.png" alt="microsoft" class="microsoft" />
+          <img src="asset/img/Group 19.png" alt="dropbox" class="dropbox" />
+          <img src="asset/img/g45263.png" alt="h and m" />
+          <img src="asset/img/logo 1 (1).png" alt="airbnb" />
+          <img src="asset/img/g3.png" alt="canon" />
+          <img src="asset/img/Group99.png" alt="dell" />
+        </section>
+
+        <section class="about">
+          <h1>
+            About <span>the Application.</span>
+          </h1>
+          <p>We have some great features from the application and it's totally free to use by all users around the world.</p>
+          <div>
+            <div class="container">
+              <div class="icon">
+                <i data-feather="phone"></i>
+              </div>
+              <h3>24/7 Support</h3>
+              <p>We have 24/7 contact support so you can contact us whenever you want and we will respond it.</p>
+            </div>
+            <div class="container main">
+              <div class="icon">
+                <i data-feather="lock"></i>
+              </div>
+              <h3>Data Privacy</h3>
+              <p>We make sure your data is safe in our database and we will encrypt any data you submitted to us.</p>
+            </div>
+            <div class="container">
+              <div class="icon">
+                <i data-feather="download"></i>
+              </div>
+              <h3>Easy Download</h3>
+              <p>Zwallet is 100% totally free to use it’s now available on Google Play Store and App Store.</p>
+            </div>
+          </div>
+        </section>
+
+        <section class="features">
+          <img src="asset/img/Group 54.png" alt="mobile-display" />
+          <div>
+            <h1>
+              All The <span>Great</span> Zwallet Features.
+            </h1>
+            <div class="container">
+              <div>
+                <span>1.</span>
+                <h5>Small Fee</h5>
+              </div>
+              <p>We only charge 5% of every success transaction done in Zwallet app.</p>
+            </div>
+            <div class="container">
+              <div>
+                <span>2.</span>
+                <h5>Data Secured</h5>
+              </div>
+              <p>All your data is secured properly in our system and it’s encrypted.</p>
+            </div>
+            <div class="container">
+              <div>
+                <span>3.</span>
+                <h5>User Friendly</h5>
+              </div>
+              <p>Zwallet come up with modern and sleek design and not complicated.</p>
+            </div>
+          </div>
+        </section>
+
+        <section class="users">
+          <h1>
+            What Users are <span>Saying.</span>
+          </h1>
+          <p>We have some great features from the application and it's totally free to use by all users around the world.</p>
+          <div>
+            <a href="facebook.com">
+              <i data-feather="arrow-left"></i>
+            </a>
+            <div>
+              <img src="asset/img/beard.png" alt="user" />
+              <h5>Alex Hansinburg</h5>
+              <p class="profesion">Designer</p>
+              <p class="testimony">
+                “This is the most outstanding app that I've ever try in my live, this app is such an amazing masterpiece and it's suitable for you who is bussy with their bussiness and must transfer money to another person aut there. Just
+                try this app and see the power!”
+              </p>
+            </div>
+            <a href="facebook.com">
+              <i data-feather="arrow-right"></i>
+            </a>
+          </div>
+        </section>
+
+        <footer>
+          <h4>Zwallet</h4>
+          <p>Simplify financial needs and saving much time in banking needs with one single app.</p>
+          <hr />
+          <div>
+            <p>2020 Zwallet. All right reserved.</p>
+            <div>
+              <p>+62 5637 8882 9901</p>
+              <p>contact@zwallet.com</p>
+            </div>
+          </div>
+        </footer>
+
+        <script>feather.replace();</script>
+      </>
+    );
+  }
+}
+
+export default Home;
