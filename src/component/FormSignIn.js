c93c902fdfbaaf4a20bfd16578ee3ed2199aa65a
Refaldy <refaldybanggana@gmail.com>
2024-02-29 16:08:29 +0700

    fix sign in page display

--- src/component/FormSignIn.js
+++ src/component/FormSignIn.js
@@ -1,21 +1,24 @@
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import * as Yup from 'yup';
-import { Formik } from 'formik';
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { register } from '../redux/asyncActions/user';
-import Alert from 'react-bootstrap/Alert';
-import InputGroup from 'react-bootstrap/InputGroup';
-import { IconContext } from 'react-icons';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { register } from "../redux/asyncActions/user";
+import Alert from "react-bootstrap/Alert";
+import InputGroup from "react-bootstrap/InputGroup";
+import { IconContext } from "react-icons";
 
-import { FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
 
 const loginSchema = Yup.object().shape({
-  username: Yup.string().required('Required').min(4, 'username to short').max(8, 'usename to long'),
-  email: Yup.string().email('invalid email format').required('Required'),
-  password: Yup.string().min(8, 'password to short').required('Required'),
+  username: Yup.string()
+    .required("Required")
+    .min(4, "username to short")
+    .max(8, "usename to long"),
+  email: Yup.string().email("invalid email format").required("Required"),
+  password: Yup.string().min(8, "password to short").required("Required"),
 });
 
 const AuthForm = ({ errors, handleSubmit, handleChange, values }) => {
@@ -26,44 +29,85 @@ const AuthForm = ({ errors, handleSubmit, handleChange, values }) => {
       {errorMsg && <Alert variant="danger">{errorMsg}</Alert>}
       <Form noValidate onSubmit={handleSubmit}>
         <InputGroup className="mb-3" controlid="formBasicUsername">
-          <InputGroup.Text id="basic-addon1">
-            <IconContext.Provider value={{ size: '1.5rem' }}>
+          <InputGroup.Text
+            id="basic-addon1"
+            className={`${errors.username && "border-danger"}`}
+          >
+            <IconContext.Provider value={{ size: "1.5rem" }}>
               <div>
                 <FiUser />
               </div>
             </IconContext.Provider>
           </InputGroup.Text>
-          <Form.Control name="username" type="text" aria-describedby="basic-addon1" value={values.username} onChange={handleChange} placeholder="Enter your username" isInvalid={!!errors.username} />
-          <Form.Control.Feedback type="invalid">{errors.username}</Form.Control.Feedback>
+          <Form.Control
+            name="username"
+            type="text"
+            aria-describedby="basic-addon1"
+            value={values.username}
+            onChange={handleChange}
+            placeholder="Enter your username"
+            isInvalid={!!errors.username}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.username}
+          </Form.Control.Feedback>
           {/* <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text> */}
         </InputGroup>
 
         <InputGroup className="mb-3" controlid="formBasicEmail">
-          <InputGroup.Text id="basic-addon2">
-            <IconContext.Provider value={{ size: '1.5rem' }}>
+          <InputGroup.Text
+            id="basic-addon2"
+            className={`${errors.email && "border-danger"}`}
+          >
+            <IconContext.Provider value={{ size: "1.5rem" }}>
               <div>
                 <FiMail />
               </div>
             </IconContext.Provider>
           </InputGroup.Text>
-          <Form.Control name="email" type="email" aria-describedby="basic-addon2" onChange={handleChange} placeholder="Enter your e-mail" isInvalid={!!errors.email} />
-          <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+          <Form.Control
+            name="email"
+            type="email"
+            aria-describedby="basic-addon2"
+            onChange={handleChange}
+            placeholder="Enter your e-mail"
+            isInvalid={!!errors.email}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.email}
+          </Form.Control.Feedback>
         </InputGroup>
 
         <InputGroup className="mb-3" controlid="formBasicPassword">
-          <InputGroup.Text id="basic-addon3">
-            <IconContext.Provider value={{ size: '1.5rem' }}>
+          <InputGroup.Text
+            id="basic-addon3"
+            className={`${errors.password && "border-danger"}`}
+          >
+            <IconContext.Provider value={{ size: "1.5rem" }}>
               <div>
                 <FiLock />
               </div>
             </IconContext.Provider>
           </InputGroup.Text>
-          <Form.Control name="password" type="password" aria-describedby="basic-addon3" onChange={handleChange} placeholder="Create your password" isInvalid={!!errors.password} />
-          <Form.Control.Feedback type="invalid">{errors.password}</Form.Control.Feedback>
+          <Form.Control
+            name="password"
+            type="password"
+            aria-describedby="basic-addon3"
+            onChange={handleChange}
+            placeholder="Create your password"
+            isInvalid={!!errors.password}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.password}
+          </Form.Control.Feedback>
         </InputGroup>
 
         <div className="d-grid">
-          <Button variant="primary" type="submit" className="fw-login-btn text-light mt-4">
+          <Button
+            variant="primary"
+            type="submit"
+            className="fw-login-btn text-light mt-4"
+          >
             Sign-Up
           </Button>
         </div>
@@ -81,14 +125,18 @@ function FormInput() {
       register({
         value,
         cb: () => {
-          navigate('/login', { replace: true });
+          navigate("/login", { replace: true });
         },
       })
     );
   };
 
   return (
-    <Formik initialValues={{ username: '', email: '', password: '' }} onSubmit={onRegister} validationSchema={loginSchema}>
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      onSubmit={onRegister}
+      validationSchema={loginSchema}
+    >
       {(props) => <AuthForm {...props} />}
     </Formik>
   );
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import Intro from '../component/Intro';
-import '../asset/css/signup.css';
-import FormInput from '../component/FormSignIn';
-import { useSelector } from 'react-redux';
-import { useNavigate, Link } from 'react-router-dom';
+import React from "react";
+import Intro from "../component/Intro";
+import "../asset/css/signup.css";
+import FormInput from "../component/FormSignIn";
+import { useSelector } from "react-redux";
+import { useNavigate, Link } from "react-router-dom";
 
 export const SignUp = () => {
   const token = useSelector((state) => state.user.token);
@@ -11,25 +11,26 @@ export const SignUp = () => {
 
   React.useEffect(() => {
     if (token) {
-      navigate('/dashboard', { replace: true });
+      navigate("/dashboard", { replace: true });
     }
   }, [navigate, token]);
 
   return (
-    <section className="row">
+    <section className="row d-flex flex-column-reverse flex-md-row">
       <Intro />
 
-      <div className="col-md-5 fw-form d-flex flex-column">
-        <h3 className="fw-motto fw-margin">Start Accessing Banking Needs With All Devices and All Platforms With 30.000+ Users</h3>
-        <p className="fw-accessibility mt-4">Transfering money is eassier than ever, you can access Zwallet wherever you are. Desktop, laptop, mobile phone? we cover all of that for you!</p>
+      <div className="col-md-5 Signup_fw-form d-flex flex-column">
+        <h3 className="fw-motto fw-margin">
+          Start Accessing Banking Needs With All Devices and All Platforms With
+          30.000+ Users
+        </h3>
+        <p className="fw-accessibility mt-4">
+          Transfering money is eassier than ever, you can access Zwallet
+          wherever you are. Desktop, laptop, mobile phone? we cover all of that
+          for you!
+        </p>
 
         <FormInput />
-
-        {/* <div class="d-grid fw-button mt-4 sign rounded">
-            <Link to="/create-pin" class="btn text-light fs-bold" type="button">
-              <span>Sign Up</span>
-            </Link>
-          </div> */}
 
         <p className="my-2 text-center fw-signup">
           Already have an account? Let's <Link to="/login">Login</Link>
